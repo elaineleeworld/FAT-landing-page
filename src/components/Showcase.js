74b866db0b1b90cae45cb222f68413273102a970
theelainelee <elaine.lee@elainel-mbp.ff0000.com>
2018-07-03 11:52:51 -0700

    video player working for showcase

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import intro_women from '../images/intro_women.mp4';
 
+console.log('VIDEO:', intro_women);
 
+// const Showcase = () => (
+//   <div className='showcase-parent'>
+//     <h1>Take a peek at some of the innovative ads we have created with the FAT framework</h1>
+//   </div>
+// )
+// export default function MyVideo(){
+// 	return <video width="300" height="250" src={intro_women} type="video/mp4" />;
+// }
 
-const Showcase = () => (
-  <div className='showcase-parent'>
-    <h1>Take a peek at some of the innovative ads we have created with the FAT framework</h1>
-  </div>
-)
+class Showcase extends Component {
+	render(){
+		console.log("TEST", intro_women);
+		return(
+			<div className='showcase-parent'>
+			    <h1>Take a peek at some of the innovative ads we have created with the FAT framework</h1>
+			    <video id="samp" width="640" height="480" controls>
+
+		        <source src={intro_women} type="video/mp4">
+		        </source>
+		      </video>
+			 </div>
+			
+			)
+	}
+}
+
 export default Showcase;
