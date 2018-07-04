3af5df6d6ce7bcbe2df9d3ef3c3378bb862a5794
theelainelee <elaine.lee@elainel-mbp.ff0000.com>
2018-07-03 17:22:56 -0700

    update circles features

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,103 +4,30 @@ import Particle from './Particle.js';
 
 const Features = () => (
 
-<div className="features-container">
-	<div>
-    <h1>Unique features of our framework</h1>
-    </div>
-  <div className='features-parent'>
-  	
-	    <div className='particles-js'>
-	    <Particle />
-	    </div>
-	    <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">NPM</div>
+		<div className="features-container">
+			<div>
+		    <h1>Unique features of our framework</h1>
 		    </div>
-		  </div>
-		</div>
+		  	<div className='features-parent'>
+		  	
+			    <div className='particles-js'>
+			    <Particle />
+			    </div>
 
-		<div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">Binary Compiling</div>
-		    </div>
-		  </div>
-		</div>
+			    <circle>NPM</circle>
+			    <circle>Binary Compiling</circle>
+			    <circle>ES6</circle>
+			    <circle>Javascript</circle>
+			    <circle>Within IAB Specs</circle>
+			    <circle>Streamlined</circle>
+			    <circle>High Volume</circle>
+			    <circle>Optimized</circle>
+			    <circle>Dynamic</circle>
+			    <circle>Greensock</circle>
+			    <circle>Efficiency</circle>
 
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">ES6</div>
-		    </div>
-		  </div>
+			  
+			 </div>
 		</div>
-
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">100% Javascript</div>
-		    </div>
-		  </div>
-		</div>
-
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">Within IAB Specs</div>
-		    </div>
-		  </div>
-		</div>
-
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">Streamlined</div>
-		    </div>
-		  </div>
-		</div>
-
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">High Volume Efficiency</div>
-		    </div>
-		  </div>
-		</div>
-
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">Optimized</div>
-		    </div>
-		  </div>
-		</div>
-
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">Dynamic Capabilities</div>
-		    </div>
-		  </div>
-		</div>
-
-		 <div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">Greensock Enabled</div>
-		    </div>
-		  </div>
-		</div>
-
-		<div className="circle">
-		  <div className="circle__inner">
-		    <div className="circle__wrapper">
-		      <div className="circle__content">Webpack</div>
-		    </div>
-		  </div>
-		</div>
-		  </div>
-		  </div>
 )
 export default Features;
