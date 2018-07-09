ca6a65bd9504def9e424dbbe44b8f8cbfcc917d2
theelainelee <elaine.lee@elainel-mbp.ff0000.com>
2018-07-09 10:27:59 -0700

    update text copy homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,33 +6,39 @@ const Home = () => (
 	<div className='home-parent'>
 	  <div className='home-top'>
 	    <h1>Faster development so you can focus on creative execution.</h1>
-	    <p>Our lightweight Javascript framework enables high-volume production of banner ads where you can create many variations and many sizes efficiently.</p>
-	    <p>INSERT IMAGE OR GIF OF MULTIPLE CREATIVE SIZES BEING MADE</p>
+	    <p>Our lightweight Javascript framework enables custom, high-quality, high-volume production of banner ads -- create many variations and many sizes efficiently. Easily migrate them to any ad-network.
+		</p>
+	
+		<p>The result is the smallest, most capable, fastest-loading ads in the industry.</p>
+	  
 	    <p>We provide common out-of-the box components (dates, video player, buttons) and enable you to manage dynamic states simply.</p>
 	  </div>
 
 	  <div className='home-middle'>
 	  	<h1>Interested? How to get started now:</h1>
 	  	<div>
-	  	<h3>STEP 1</h3>
-	  	<li><a href="https://github.com/ff0000-ad-tech/tmpl-standard-base" target="_blank" rel="noopener noreferrer">TEMPLATE </a><br/>Use a template already made for you</li>
+	  	<h3>STEP 1: TEMPLATE</h3>
+	  	<li><a href="https://github.com/ff0000-ad-tech/tmpl-standard-base" target="_blank" rel="noopener noreferrer">Start with a standard DCM template</a><br/>Need other ad-networks? No problem, we have templates for DC Studio, Sizmek, FlashTalking & more.</li>
 	  	</div>
 	  	<div>
-	  	<h3>STEP 2</h3>
-	  	<li><a href="https://github.com/ff0000-ad-tech/wp-creative-server/blob/master/README.md" target="_blank" rel="noopener noreferrer">SERVER</a> <br/>start the Creative Server</li>
+	  	<h3>STEP 2: TOOLING</h3>
+	  	<li><a href="https://github.com/ff0000-ad-tech/wp-creative-server/blob/master/README.md" target="_blank" rel="noopener noreferrer">Use Creative Server to do the hard stuff</a> <br/>Zero-configuration Webpack/ES6, watch-processes, localhosts, & compiling.</li>
 	  	</div>
 	  	<div>
-	  	<h3>STEP 3</h3>
-	  	<li><a href="https://ff0000-ad-tech.github.io/ad-docs/" target="_blank" rel="noopener noreferrer">USE FAT FRAMEWORK DOCS HERE</a><br/> and FOCUS ON DESIGN <br/>start a server watch process. Let your creative juices flow!</li>
+	  	<h3>STEP 3: AUTHORING</h3>
+	  	<li><a href="https://ff0000-ad-tech.github.io/ad-docs/" target="_blank" rel="noopener noreferrer">Write the FAT Framework & Greensock (or use your preferred libraries)</a><br/>se your budgets for DESIGN, not tooling -- anything that's possible in an IAB container can be built!</li>
 	  	</div>
 	  	<div>
-	  	<h3>STEP 4</h3>
-	  	<li>DELIVERABLE <br/>with your finished banner ad, publish a ready-to-go, traffic deliverable</li>
+	  	<h3>STEP 4: DELIVERIES</h3>
+	  	<li>Compile & deliver to traffic.  Generate all of your targets <i>without</i> code redundancy!</li>
+	  	<br/>
+	  	<p>FAT is ready-to-go, as-is.</p>
+	  
+	  	<p>It is also modular</p>
+	  	
+	  	<p>Click to the Features page for pieces that might benefit your process.</p>
 	  	</div>
-	  	<div>
-	  	<h3>Working with DCS, DCM, FlashTalking, Sizmek</h3>
-	  	<li>Have specific ad networks you are working with?  No problem, we have templates already configured for you</li>
-	  	</div>
+	  	
 	  </div>
 
 	  <h1>Try Creative Server and the FF0000 FAT Framework together, or just use Creative Server in additon to your current build process</h1>
