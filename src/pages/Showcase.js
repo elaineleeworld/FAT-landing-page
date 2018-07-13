15c77f9565bc5e14bb1a5b0e3cabe8c4c936fd78
theelainelee <elaine.lee@elainel-mbp.ff0000.com>
2018-07-13 14:41:43 -0700

    update showcase render video array

--- src/pages/Showcase.js
+++ src/pages/Showcase.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import intro_women from '../images/intro_women.mp4';
+import bats from '../images/bats.mp4';
 
 
-function renderVideo(props){
-	const videos = props.videoSrc;
-	const renderVideo = videos.map((videoSrc) =>
-		<source src={videoSrc} type="video/mp4"/>    
-		      );
-		    
-};
 
-const videos = [intro_women, intro_women, intro_women];
+const videos = [intro_women, bats];
+
+const renderVideo = videos.map((videoSrc) =><video id="samp"  controls><source src={videoSrc} type="video/mp4"/></video>);
 
 
 
@@ -21,7 +17,8 @@ class Showcase extends Component {
 		return(
 			<div className='showcase-parent'>
 			    <h1>Take a peek at some of the innovative ads we have created with the FAT framework</h1>
-			    <video id="samp"  controls>{renderVideo}</video>
+			    <div>{renderVideo}</div>
+			    
 			    
 			 </div>
 			
