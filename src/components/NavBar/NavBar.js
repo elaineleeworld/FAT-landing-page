3cbede44c338b819e364e0c88f63f4de8260dde1
theelainelee <elaine.lee@elainel-mbp.ff0000.com>
2019-01-16 15:20:53 -0800

    sticky nav and onpage scroll to component section working

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import createBrowserHistory from 'history';
 
 import { render } from 'react-dom';
 import './NavBar.css';
@@ -13,12 +14,65 @@ class NavBar extends React.Component{
 	constructor(props){
 		super(props);
 		this.main = React.createRef();
+		// this.state={};
+
+		// this.stickyNav = this.stickyNav.bind(this);
 	}
+
+	// stickyNav(){
+	// 	this.setState({scroll: window.scrollY});
+	// }
+
+	//  componentDidMount() {
+	// 	const el = document.querySelector('nav');
+	// 	this.setState({top: el.offsetTop, height: el.offsetHeight});
+	// 	window.addEventListener('scroll', this.handleScroll);
+	// }
+  
+ //  componentDidUpdate() {
+	// 	this.state.scroll > this.state.top ? 
+	// 		document.body.style.paddingTop = `${this.state.height}px` :
+	// 		document.body.style.paddingTop = 0;
+	// }
+
+	 handleScroll = e => {
+    e.preventDefault();
+    const main = this.main.current;
+    window.scrollTo({
+      top: 600,
+      left: 0,
+      behavior: "smooth"
+    });
+}
+	 handleScrollabout = e => {
+    e.preventDefault();
+    const main = this.main.current;
+    window.scrollTo({
+      top: 3600,
+      left: 0,
+      behavior: "smooth"
+    });
+}
+    handleScrollhome = e => {
+    e.preventDefault();
+    const main = this.main.current;
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+}
+	
+	// navigateHome(props){
+		
+	// 	this.props.history.push('/');
+	// }
+  
 
 	render(){
 		return(
 			<header className='navbar'>
-		<nav className='navbar__container'>
+		<nav className='navbar__container'  >
 		
 			<div className='navbar__container-logo'>
 			
@@ -36,9 +90,9 @@ class NavBar extends React.Component{
 			<div className='spacer'></div>
 			<div className='navbar__container-items'>
 				<ul>
-					<li><Link to='/'>HOME</Link></li>
-					<li><Link to='/'>FEATURES</Link></li>
-					<li><Link to='/'>ABOUT</Link></li>
+					<li><Link ref={this.main} onClick={this.handleScrollhome} to='/'>HOME</Link></li>
+					<li><Link ref={this.main} onClick={this.handleScroll} to='/'>FEATURES</Link></li>
+					<li><Link ref={this.main} onClick={this.handleScrollabout} to='/'>ABOUT</Link></li>
 					<li><Link to='/showcase'>SHOWCASE</Link></li>
 				</ul>
 			</div>
