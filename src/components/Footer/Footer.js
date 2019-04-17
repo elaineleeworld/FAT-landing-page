97932627bfc76c8f05f39dceb07abea3ef2b1290
Elaine Lee <elaine.lee@ff0000.com>
2019-04-17 16:12:48 -0700

    update footer link functionality

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,27 +4,57 @@ import FooterLogo from '../../images/footer_logo.png'
 import InstagramLogo from '../../images/instagram_logo.png'
 import TwitterLogo from '../../images/twitter_logo.png'
 import LinkedInLogo from '../../images/linkedin_logo.png'
+import { Redirect, withRouter } from 'react-router'
 import { Link } from 'react-router-dom'
 
 class Footer extends Component {
+
+	constructor(props) {
+		super(props)
+		// console.log('CURRENT LOCATION', props.location)
+
+		// this.state = {
+		// 	homePage: false
+		// }
+
+		this.handleScrollHome = this.handleScrollHome.bind(this)
+		this.handleScrollFeatures = this.handleScrollFeatures.bind(this)
+		this.handleScrollAbout = this.handleScrollAbout.bind(this)
+	}
+
+	handleScrollHome() {
+		// console.log('HOMEPAGE',this.state.homePage)
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		})
+	}
+	handleScrollFeatures() {
+		
+		var element = document.getElementById('headline-style-topfeatures')
+		element.scrollIntoView({ behavior: 'smooth' })
+		window.scrollTo({
+			top: element.offsetTop - 120,
+			behavior: 'smooth'
+		})
+	
+	}
+	handleScrollAbout() {
+		
+		// window.scrollTo(0, document.body.scrollHeight);
+		var element = document.getElementById('homepage-about-red')
+		element.scrollIntoView({ behavior: 'smooth' })
+	}
 	render() {
 		return (
 			<footer className="App-footer">
 				<div className="container">
 					<div className="footer__link__items">
 						<ul>
-							<li>
-								<Link to="/">Home</Link>
-							</li>
-							<li>
-								<Link to="/features">Features</Link>
-							</li>
-							<li>
-								<Link to="/about">About</Link>
-							</li>
-							<li>
-								<Link to="/showcase">Showcase</Link>
-							</li>
+							<li onClick={this.handleScrollHome}>Home</li>
+							<li onClick={this.handleScrollFeatures}>Features</li>
+							<li onClick={this.handleScrollAbout}>About</li>
+							<li><a href="http://sizzle.wearered.com/">Showcase</a></li>
 						</ul>
 					</div>
 
